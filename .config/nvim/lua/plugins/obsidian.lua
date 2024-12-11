3e1032c8c9b6ab717b5298e0a93f92d52aa1d1a1
Nikita Elfimov <nelfimov@mail.ru>
2024-12-11 18:54:27 +0300

    feat(config): nvim (#1)

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,47 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "work",
+        path = "~/Documents/Dev/Obsidian/work",
+      },
+    },
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "open", url })
+    end,
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    file_ignore_patterns = { "^.docker/", "^.yarn/" },
+  },
+}
