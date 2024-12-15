11f1948e9dedeae6f2c1a687ea991c5b77325de1
Nikita Elfimov <nelfimov@mail.ru>
2024-12-15 19:23:10 +0300

    feat(nvim): bump to lazy 14 (#5)

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,3 +8,7 @@ vim.g.lazyvim_eslint_auto_format = false
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+vim.g.snacks_animate = false
+
+vim.g.lazyvim_picker = "fzf"
