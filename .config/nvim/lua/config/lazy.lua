32948e6a4ccca19e2d6ae7b839702659ce882fee
Nikita Elfimov <nelfimov@mail.ru>
2024-12-18 15:13:46 +0300

    feat(nvim): enable harpoon

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "plugins" },
   },
   defaults = {
