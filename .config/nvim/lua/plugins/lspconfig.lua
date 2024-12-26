1b7d1c8780102e6543ac4f7b714d3cbe8519ebc7
Nikita Elfimov <nelfimov@mail.ru>
2024-12-26 21:12:45 +0300

    feat(common): update

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -13,5 +13,13 @@ return {
     end
 
     opts.autoformat = false
+
+    local function find_terraform_root(fname)
+      local root = require("lspconfig.util").root_pattern(".terraform", ".terraform.lock.hcl")(fname)
+      return root or require("lspconfig.util").find_git_ancestor(fname)
+    end
+
+    opts.servers.terraformls = opts.servers.terraformls or {}
+    opts.servers.terraformls.root_dir = find_terraform_root
   end,
 }
