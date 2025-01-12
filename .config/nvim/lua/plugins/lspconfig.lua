3a23ce52b4d58ec42452de21acafdb7e231abff7
Nikita Elfimov <nelfimov@mail.ru>
2025-01-12 21:24:26 +0300

    Feat/move to studio (#18)
    * feat(config): move to studio
    
    * chore: github

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    local nvim_lsp = require("lspconfig.util")
-
     local function path_exists(project_root, path)
-      return vim.loop.fs_stat(project_root .. path) ~= nil
+      return vim.loop.fs_stat(project_root or "" .. path) ~= nil
     end
 
-    local project_root = nvim_lsp.root_pattern(".git")(vim.fn.getcwd())
+    local project_root = require("lspconfig.util").root_pattern(".git")(vim.fn.getcwd())
     local tsdk_path = "/.yarn/sdks/typescript/lib"
 
     if path_exists(project_root, tsdk_path) then
@@ -19,7 +17,7 @@ return {
 
     local function find_terraform_root(fname)
       local root = require("lspconfig.util").root_pattern(".terraform", ".terraform.lock.hcl")(fname)
-      return root or require("lspconfig.util").find_git_ancestor(fname)
+      return root or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
     end
 
     opts.servers.terraformls = opts.servers.terraformls or {}
