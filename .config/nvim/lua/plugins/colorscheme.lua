f55f174bacba67733d363f6f37e2d957823d08b2
Nikita Elfimov <nelfimov@mail.ru>
2025-02-13 09:52:06 +0300

    feat(nvim): colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
-  { "Mofiqul/dracula.nvim" },
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      overrides = {
+        SnacksPickerDir = { fg = "#F8F8F2" },
+        SnacksPickerPathHidden = { fg = "#F8F8F2" },
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
