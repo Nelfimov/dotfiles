5fec68fc3cbf2c32456ac3f928e57f23becabdba
Nikita Elfimov <nelfimov@mail.ru>
2025-02-17 09:28:53 +0300

    feat(nvim): use dracula

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,6 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
-    ---@module 'dracula'
-    ---@type DraculaConfig
-    opts = {
-      italic_comment = true,
-      overrides = function(colors)
-        return {
-          SnacksPickerDir = { fg = colors.comment },
-          SnacksPickerPathHidden = { fg = colors.comment },
-          SnacksPickerPathIgnored = { fg = colors.comment },
-        }
-      end,
-    },
+    "dracula/vim",
   },
   {
     "LazyVim/LazyVim",
