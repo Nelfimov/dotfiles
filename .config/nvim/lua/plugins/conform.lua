c36f6798531a6efe87d98041626436301cd4e355
Nikita Elfimov <en@atls.team>
2025-08-14 15:42:18 +0300

    feat(neovim): rustfmt as default

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,9 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters_by_ft = {
+      rust = { "rustfmt", lsp_format = "fallback", default_edition = "2024" },
+    },
     formatters = {
       prettier = {
         args = function(self, ctx)
