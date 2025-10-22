5a9cec5a241838d1ff817e6a977f501423cdcf00
Nikita Elfimov <nelfimov@mail.ru>
2025-10-22 11:44:16 +0300

    feat(neovim): disable tabs

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      show_buffer_close_icons = false,
+      show_tab_indicators = false,
+    },
+  },
+}
