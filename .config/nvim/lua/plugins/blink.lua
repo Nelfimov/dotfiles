73d617f078d44c65c343495f13b8ce33714aff0a
Nikita Elfimov <nelfimov@mail.ru>
2025-10-28 20:46:08 +0300

    feat(neovim): scroll keymap

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     keymap = {
       ["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
     },
   },
 }
