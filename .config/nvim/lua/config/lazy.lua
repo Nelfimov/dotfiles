2327d8fd9c6453637ea6b15948af44b5bab56e7b
Nikita Elfimov <nelfimov@mail.ru>
2025-10-31 19:16:11 +0300

    feat(neovim): enable avante

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -43,6 +43,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
     { import = "lazyvim.plugins.extras.ui.smear-cursor" },
+    { import = "lazyvim.plugins.extras.ai.avante" },
     { import = "plugins" },
   },
   defaults = {
