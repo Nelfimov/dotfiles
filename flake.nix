d2886128d001ba3ccc3fa86bdbd002c9eb0fd6b2
Nikita Elfimov <nelfimov@mail.ru>
2025-11-17 11:16:55 +0300

    feat(common): rm borders

--- flake.nix
+++ flake.nix
@@ -80,7 +80,6 @@
             brews = [
               "mas"
               "solargraph"
-              "borders"
               "tree-sitter-cli" # neovim LSP
               "nixfmt" # nix
             ];
