5e83cf3c5da9da7d7134cc65b7de3c3e7696d798
Nikita Elfimov <nelfimov@mail.ru>
2026-02-06 07:48:49 +0300

    feat(nvim): markdown disable line length

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -4,6 +4,12 @@ return {
   opts = {
     linters_by_ft = {
       php = { "phpcs", "phpstan" },
+      markdown = { "markdownlint-cli2" },
+    },
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", vim.fn.stdpath("config") .. "/markdownlint.json" },
+      },
     },
   },
 }
