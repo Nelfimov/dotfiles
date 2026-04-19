2e7f1cebc07850748b714d5ab5520b22b27375ef
Nikita Elfimov <nelfimov@mail.ru>
2026-04-19 11:08:50 +0300

    feat(qmk): use new app with ui

--- qmk/setup.sh
+++ qmk/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # For ergohaven K:03 RuEn mode
+curl -fsSLO https://github.com/VlaDi4eKK/qmk-hid-host/releases/download/latest/QMK-HID-Host-macos-app.zip
 
-cp qmk-hid-host.plist ~/Library/LaunchAgents/
+unzip QMK-HID-Host-macos-app.zip
+rm QMK-HID-Host-macos-app.zip
+
+cp 'QMK HID Host.app' ~/Applications/
