a9bd841fd1dec7ba397b3ba1190ec59ac9919e12
Nikita Elfimov <nelfimov@mail.ru>
2026-05-13 07:54:25 +0300

    feat(neovim): update, add fff

---
+++ .config/nvim/lua/plugins/fff.lua
@@ -0,0 +1,49 @@
+return {
+  "dmtrKovalenko/fff.nvim",
+  build = function()
+    require("fff.download").download_or_build_binary()
+  end,
+  opts = {
+    git = {
+      status_text_color = false,
+    },
+  },
+  lazy = false, -- the plugin lazy-initialises itself
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "FFFind files",
+    },
+    {
+      "<leader>ff",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "FFFind files",
+    },
+    {
+      "<leader>/",
+      function()
+        require("fff").live_grep()
+      end,
+      desc = "LiFFFe grep",
+    },
+    {
+      "<leader>fz",
+      function()
+        require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+      end,
+      desc = "Live fffuzy grep",
+    },
+    {
+      "<leader>fc",
+      function()
+        require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+      end,
+      desc = "Search current word",
+    },
+  },
+}
