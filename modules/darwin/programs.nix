7a8d19d4536200cbd71b874c31113b6310500a58
Nikita Elfimov <nelfimov@mail.ru>
2026-06-24 15:31:17 +0300

    feat(nix): enable watch auth

--- modules/darwin/programs.nix
+++ modules/darwin/programs.nix
@@ -1,12 +1,15 @@
 {
   security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.watchIdAuth = true;
 
-  programs.zsh = {
-    enable = true;
-    enableSyntaxHighlighting = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
+  programs = {
+    zsh = {
+      enable = true;
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+    };
+
+    direnv.enable = true;
   };
-
-  programs.direnv.enable = true;
 }
