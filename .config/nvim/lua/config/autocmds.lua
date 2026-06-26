d287b987d9636704e4e451ae600b4a8b09906b94
Nikita Elfimov <nelfimov@mail.ru>
2026-06-26 22:12:48 +0300

    feat(neovim): init autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,4 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
--- Disable autoformat for lua files
+-- Disable autoformat for ruby files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "ruby", "rb" },
   callback = function()
@@ -82,6 +78,27 @@ vim.api.nvim_create_autocmd("User", {
       end
 
       schedule_sync()
+    end
+  end)(),
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ObsidianNoteEnter",
+  callback = (function()
+    local vault = vim.fn.expand("~/Documents/Dev/Obsidian")
+    local uv = vim.uv or vim.loop
+    local timer
+    local running = false
+    local rerun = false
+
+    local function notify(msg, level)
+      vim.schedule(function()
+        vim.notify(msg, level or vim.log.levels.INFO, { title = "Obsidian Git" })
+      end)
+    end
+
+    return function(ev)
+      notify("HELLO AND WELCOME")
     end
   end)(),
 })
